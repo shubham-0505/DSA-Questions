d4150dd1eb8d667f501e1096b80856a09c50a93f
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2026-06-07 00:04:39 +0530

    Create BinaryToDecimal.cpp

---
+++ BinaryToDecimal.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter a binary number: ";
+    cin>>n;
+
+    int ans = 0;
+    int i = 0;
+
+    while(n!=0){
+        int digit = n%10;
+
+        if(digit==1){
+            ans = pow(2,i) + ans;
+        }
+        n = n/10;
+        i++;
+    }
+    cout<<"Decimal representation of the number is: "<<ans<<endl;
+}
