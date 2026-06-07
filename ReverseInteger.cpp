225bb6c138458ff5fbac6eae748b101e835a7a53
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2026-06-07 18:46:13 +0530

    Create ReverseInteger.cpp

---
+++ ReverseInteger.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int ans = 0;
+
+    while(n!=0){
+        int digit = n%10;
+
+        // if((ans>INT8_MAX/10) || (ans<INT8_MIN/10)){
+        //     return 0;
+        // }
+        
+        ans = (ans * 10) + digit;
+        n = n/10;
+    }
+    cout<<"Reverse of the given number is: "<<ans<<endl;
+    return ans;
+}
