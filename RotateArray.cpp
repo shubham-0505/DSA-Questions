c937c6d85e12fec5d6e1e1cce1bfb2be5458cd9b
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2026-06-29 12:01:00 +0530

    Create RotateArray.cpp

---
+++ RotateArray.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void rotate(vector<int> &arr,int n,int k){
+
+    vector<int> temp(n);
+
+    for(int i=0;i<n;i++){
+        temp[(i+k)%n] = arr[i];
+    }
+
+    arr = temp;
+}
+
+void printArray(vector<int> &arr,int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    vector<int> num = {1,2,3,4,5};
+
+    cout<<"The given array is: ";
+    printArray(num,5);
+
+    int steps;
+    cout<<"Enter the no of steps to rotate array: ";
+    cin>>steps;
+
+    rotate(num,5,steps);
+
+    cout<<"The rotated array by "<<steps<<" steps is: ";
+    printArray(num,5);
+
+    return 0;
+}
