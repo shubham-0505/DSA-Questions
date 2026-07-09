e0e496c7c54642c282398f4dd1a10edfea64f92a
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2026-07-09 15:32:45 +0530

    Create Pointers.cpp

---
+++ Pointers.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num = 50;
+
+    // Creating a pointer that will store the address of num
+    int *p = &num;
+
+    cout<<"Address of num is: "<<&num<<endl;
+    cout<<"Value of num is: "<<*p<<endl;
+
+    cout<<"The size of integer is: "<<sizeof(num)<<endl;
+    cout<<"The size of pointer to the integer is: "<<sizeof(p)<<endl;
+
+    // Copying a pointer into another
+    int *q = p;
+    cout<<"The value of second pointer is: "<<*q<<endl;
+    cout<<"The address stored in second pointer is: "<<q<<endl;
+
+    // Incrementing a pointer
+    *p = *p + 1;
+    cout<<"New value of num is: "<<*p<<endl;
+
+    return 0;
+}
