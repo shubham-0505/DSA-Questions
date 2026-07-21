015f9068eeb95c73ebb7b84d3073ac5c9a05ea7f
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2026-07-21 22:37:04 +0530

    Create FibonacciSeriesUsingRecursion.cpp

---
+++ FibonacciSeriesUsingRecursion.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int ans = fibonacci(n);
+    cout<<"The "<<n<<" th term in fibonacci series is: "<<ans<<endl;
+    
+    return 0;
+}
